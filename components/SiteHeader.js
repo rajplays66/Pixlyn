22046f04528ffab23ef88848549766fae0ffdf2e
rajplays66 <rajplays66@gmail.com>
2026-09-19 19:27:45 +0600

    Add SiteHeader component with header markup

---
+++ components/SiteHeader.js
@@ -0,0 +1,9 @@
+export default function SiteHeader() {
+  return (
+    <header className="w-full flex justify-center pt-7 pb-2">
+      <h1 className="font-handwritten text-5xl sm:text-6xl text-ink tracking-wide select-none">
+        Pixlyn
+      </h1>
+    </header>
+  );
+}
